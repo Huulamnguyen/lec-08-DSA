49ae4b0a879586aedfbfe03a3239215b8031df5e
huulamnguyen <liamnguyen.swe@gmail.com>
2022-06-02 23:11:29 -0400

    continue working on Question 03

--- question-03/app.js
+++ question-03/app.js
@@ -41,24 +41,57 @@ class singlyLinkedList{
 
     //todo: should insert a new node at a given position
 
-    // insert(val, position){
-    //     if(!this.head){
-    //         this.head = new Node(val);
-    //         return;
-    //     }
+    getLast() {
+        if (!this.head) {
+            return null;
+         }
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
+    }
 
-    //     if(position === 0){
-    //         this.head = new Node(val, this.head);
-    //         return;
-    //     }
 
-    //     const previous = this.
-    // }
+    getAt(index) {
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            if (counter === index) {
+            return node;
+            }
+            counter++;
+            node = node.next;
+        }
+        return null;
+    }
+    
+
+    insert(val, position){
+        if(!this.head){
+            this.head = new Node(val);
+            return;
+        }
+
+        if(position === 0){
+            this.head = new Node(val, this.head);
+            return;
+        }
+
+        const previous = this.getAt(position - 1) || this.getLast();
+        const node = new Node(val, previous.next);
+        previous.next = node; 
+
+    }
     
 }
 
 let node = new singlyLinkedList()
-node.append(3)
 node.append(4)
-node.append(5)
+// node.append(4)
+// node.append(5)
+node.insert(5, 1);
+node.insert(6, 2);
 console.log(node);
