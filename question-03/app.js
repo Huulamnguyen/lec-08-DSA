4df5147317593a7181217fa0c18a330e2986a917
huulamnguyen <liamnguyen.swe@gmail.com>
2022-06-03 07:51:31 -0400

    Completed Question 03

--- question-03/app.js
+++ question-03/app.js
@@ -53,22 +53,8 @@ class singlyLinkedList{
             node = node.next;
         }
     }
-
-
-    getAt(index) {
-        let counter = 0;
-        let node = this.head;
-        while (node) {
-            if (counter === index) {
-            return node;
-            }
-            counter++;
-            node = node.next;
-        }
-        return null;
-    }
     
-
+    //todo: should insert a new node at a given position
     insert(val, position){
         if(!this.head){
             this.head = new Node(val);
@@ -80,12 +66,68 @@ class singlyLinkedList{
             return;
         }
 
-        const previous = this.getAt(position - 1) || this.getLast();
+        const previous = this.nodeAtIndex(position - 1) || this.getLast();
         const node = new Node(val, previous.next);
         previous.next = node; 
-
     }
-    
+
+    //todo: should remove the node with the given value from the list
+    remove(val){
+        let temp = this.head;
+        let prev = null;
+        if(temp != null && temp.val == val){
+            this.head = temp.next;
+            return;
+        }
+
+        while(temp != null && temp.val != val){
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if(temp == null){
+            return;
+        }
+        prev.next = temp.next;
+    }
+
+    //todo: should return the length of the linked list
+    size() {
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            counter++;
+            node = node.next;
+        }
+        return counter;
+    }
+
+    //todo: should return the node at the given index; if not found, return -1
+    nodeAtIndex(index) {
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            if (counter === index) {
+            return node;
+            }
+            counter++;
+            node = node.next;
+        }
+        return -1;
+    }
+
+    //todo: should print the list in this format:
+    //todo: `${head node value} => ${node value} … => ${tail node value}`
+    printList(){
+        const my_arr = [];
+        let current = this.head;
+        while (current != null) {
+            my_arr.push(current.val);
+            current = current.next;
+        }
+        const string = my_arr.join("=>");
+        console.log(string);
+    }
 }
 
 let node = new singlyLinkedList()
@@ -94,4 +136,8 @@ node.append(4)
 // node.append(5)
 node.insert(5, 1);
 node.insert(6, 2);
-console.log(node);
+// node.remove(4)
+console.log(node.size())
+console.log(node.nodeAtIndex(0)) // should return node val 4
+console.log(node.printList())
+// console.log(node);
